96c16917dcdd80492a2722b8f39438065b4e50b3
coder_fu <1161789067@qq.com>
2020-03-03 17:35:15 +0800

    加了扫包操作，可以运行

--- movie-pcweb/src/main/java/edu/nf/movie/MonsterMovieApplication.java
+++ movie-pcweb/src/main/java/edu/nf/movie/MonsterMovieApplication.java
@@ -1,5 +1,6 @@
 package edu.nf.movie;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020-03-02
  */
 @SpringBootApplication
+@MapperScan("edu.nf.movie.customer.dao")
 public class MonsterMovieApplication {
 
     public static void main(String[] args) {
